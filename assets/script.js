5737eab4a5a10ba47194b2cfc694eb352dc68899
bearsanity <jsweeney96@hotmail.com>
2025-11-28 21:04:39 -0330

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+ $(() => {
+  "use strict";
+
+
+ 
+//Testing to see if scraped duck works
+ 
+//  const url = "https://raw.githubusercontent.com/bigfoott/ScrapedDuck/data/events.min.json";
+
+//   async function testScrapedDuck() {
+//     const response = await fetch(url);
+//     const events = await response.json();
+//     console.log("All Events:", events);
+
+//     const latestEvent = events.reduce((a, b) =>
+//       new Date(b.end) > new Date(a.end) ? b : a
+//     );
+
+//     console.log("Latest Event:", latestEvent);
+//   }
+
+//   testScrapedDuck();
+
+
+});
