8d5d863c654af840dc6f3e4f82809ef4aa647011
bearsanity <jsweeney96@hotmail.com>
2025-11-30 23:17:50 -0330

    added working scraped duck api call + map from raw event data to calender data

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,96 @@
  $(() => { //jquery reads this as the same way as document.addEventListener('DOMContentLoaded', () => {
   "use strict";
+   
+    //---------------
+    //API Urls & Keys 
+    //---------------
+    const apiKey = 'e26922b5bf8ae6d46c0de5695047bfd2';
+    //const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+    //const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+    //const duckUrl = 'https://raw.githubusercontent.com/bigfoott/ScrapedDuck/data/events.min.json';
 
-//Testing to see if scraped duck works
- 
-//  const url = "https://raw.githubusercontent.com/bigfoott/ScrapedDuck/data/events.min.json";
 
-//   async function testScrapedDuck() {
-//     const response = await fetch(url);
-//     const events = await response.json();
-//     console.log("All Events:", events);
-
-//     const latestEvent = events.reduce((a, b) =>
-//       new Date(b.end) > new Date(a.end) ? b : a
-//     );
-
-//     console.log("Latest Event:", latestEvent);
-//   }
-
-//   testScrapedDuck();
+    //-------------------
+    //Hooks into the page
+    //-------------------
 
     const updateButton = $('#update-button');
     const cityInput = $('#city-input');
 
- //Calendar from FullCalendar
+    //--------------------------
+    //Calendar from FullCalendar
+    //--------------------------
 
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth'
+        initialView: 'dayGridMonth',
+        events: 
+
     });
     
     calendar.render();
-      
+
+    //----------------
+    //Helper functions
+    //----------------
+    
+    //Scrapped duck API link
+    const duckUrl = "https://raw.githubusercontent.com/bigfoott/ScrapedDuck/data/events.min.json";
+
+    //Calls scrapped duck API to get raw event data then transforms it into an array that the Calendar can read
+    async function getScrappedDuckEvents() {
+        const response = await fetch(duckUrl);
+        const rawEvents = await response.json();
+        console.log("All Events:", rawEvents);
+
+        //This maps the raw data to an array the Calender can read
+        const calenderEvents = rawEvents.map(event =>({
+            title: event.name,
+            start: event.start,
+            end: event.end,
+            url: event.link,
+        }))
+
+        return calenderEvents;
+        };
+
+        console.log(getScrappedDuckEvents());
+   
+   
+    //Creates the card for current weather
+    function renderCurrentWeather(weatherInfo) {
+        const container = $('#current-weather');
+        container.empty(); //To clear out old content if not already empty
+        
+        const current = weatherInfo.list[0]; //cleaner than using weatherInfo.list[0] over and over
+        
+        const city = weatherInfo.city.name;
+        const temp = (current.main.temp).toFixed(1);
+        const humidity = (current.main.humidity).toFixed(0);
+        const wind = (current.wind.speed * 3.6).toFixed(2);//to change it to km/h
+        const icon = current.weather[0].icon; //gives the icon code
+    };
+
+    
+    //--------------
+    //Main Functions
+    //--------------
+    
+    //Saving the search to localstorage
+    function saveCityToHistory(city) {
+        let history = JSON.parse(localStorage.getItem('history')) || []; // [] is incase nothing exists yet
+
+        //To prevent duplicates | .some() checks if any element in the array matches the given condition
+        const normalizedCity = city.toLowerCase();
+            if (!history.some(c => c.toLowerCase() === normalizedCity)) {
+            history.push(city);
+            localStorage.setItem('history', JSON.stringify(history));
+        }
+    };
+
+    //---------------
+    //Event listeners
+    //---------------
+
+    //updateButton.on('click', )
 });
