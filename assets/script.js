8a19c5d722b5bdc148c385c89f6a8708f23552fe
bearsanity <jsweeney96@hotmail.com>
2025-12-05 20:02:53 -0330

    added weather functionality

--- assets/script.js
+++ assets/script.js
@@ -13,12 +13,20 @@
     //-------------------
     //Hooks into the page 
     //-------------------
-
+    
     const updateButton = $('#update-button');
     const cityInput = $('#city-input');
     var calendarEl = document.getElementById('calendar');
     let calendar; //Defined as null, to be updated with renderCalendar. Defined here so it can be accessed by other functions
 
+    
+    //==========================================================================================
+    //--------   Weather Functions    ---------
+    //==========================================================================================
+    
+    
+    
+    
     //----------------------------
     //Helper functions + constants
     //----------------------------
@@ -33,7 +41,7 @@
         'event': '#78d7ff',
         'go-battle-league': '#1e64d5',
         'go-pass': '#928fb8',
-        'max-battles': '#5b537d',
+        'max-battles': '#8c80c0',
         'max-mondays': '#c6d831',
         'pokemon-go-tour': '#77b02a',
         'pokemon-spotlight-hour': '#429058',
@@ -50,6 +58,7 @@
         // '#032769'
     };
     console.log(eventColors);
+
     //Scrapped duck API link
     const duckUrl = "https://raw.githubusercontent.com/bigfoott/ScrapedDuck/data/events.min.json";
 
@@ -97,19 +106,65 @@
         return uniqueEventTypesArray; 
     };
    
+    
+    //To be called after getCoordinates has retrieved the coordinates of the city
+    async function getWeather(latitude, longitude) {
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+
+        try { //Let's you attempt code that might fail without crashing the entire script. 
+            const response = await fetch(weatherUrl);
+            if (!response.ok) {
+                throw new Error('Invalid response from server');
+            }
+            const data = await response.json();
+            console.log('Weather data:', data);
+            return data;
+        
+        } catch(err) {
+            alert('There was an error finding the weather.');
+            console.error(err);
+        };
+    };
    
-    //Creates the card for current weather
-    function renderCurrentWeather(weatherInfo) {
-        const container = $('#current-weather');
-        container.empty(); //To clear out old content if not already empty
+    //gets location coordinates for getWeather
+    async function getCoordinates(city){
+        try { 
+            const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+            const response = await fetch(geoUrl);
+            
+            if (!response.ok) {
+                throw new Error("Bad response from server");
+            }
+            
+            const data = await response.json();
+            console.log('Data:', data); //console.log to see that it's working
+
+            //If the api returns bad data or no data it will end the process and throw an alert
+            if (!Array.isArray(data) || data.length === 0) {
+                alert('City not found. Please try searching again.');
+                return;
+            }
         
-        const current = weatherInfo.list[0]; //cleaner than using weatherInfo.list[0] over and over
-        
-        const city = weatherInfo.city.name;
-        const temp = (current.main.temp).toFixed(1);
-        const humidity = (current.main.humidity).toFixed(0);
-        const wind = (current.wind.speed * 3.6).toFixed(2);//to change it to km/h
-        const icon = current.weather[0].icon; //gives the icon code
+        //limit = 1 in the api means only 1 result is returned, but it always returns an array so data[0] is necessary
+            const latitude = data[0].lat;
+            const longitude = data[0].lon;
+
+            console.log('Lat:', latitude);
+            console.log('Lon:', longitude);
+
+
+            return {lat: latitude, lon: longitude};
+
+        } catch (err) {
+            alert('There was an error finding the city coordinates. Please try again.');
+            console.error(err);
+        }
+    };
+
+    function addWeatherToCalendar(weatherEvents) {
+        weatherEvents.forEach((event) => {
+        calendar.addEvent(event)
+        });
     };
 
     function toggleEventDisplay(eventToRemove) {
@@ -117,8 +172,8 @@
         for (let i = 0; i < events.length; i++) {
             if (events[i].extendedProps.eventType === eventToRemove) { //Because eventTypes is a custom property, the calendar saves it under extendedProps
                if (events[i].display === 'none') {
-                    events[i].setProp('display', 'auto'); //Set prop is the calendar method to change a property
-                }   else {events[i].setProp('display', 'block');
+                    events[i].setProp('display', 'block'); //Set prop is the calendar method to change a property
+                }   else {events[i].setProp('display', 'none');
                     }
             }
         }
@@ -140,6 +195,28 @@
         }
     };
 
+    //Gets 5 day forecast in event object form so the calendar can read it
+    function getWeatherEvents(weatherInfo) {
+        // These indices give roughly noon for each of the next 5 days
+        const indices = [7, 15, 23, 31, 39];
+        const weatherEvents = indices.map(i => {
+            const dayData = weatherInfo.list[i];
+            if (!dayData) {
+                return null; 
+            }
+            
+            return {
+                eventType: 'weather',
+                //Calendar needs date in ISO form
+                start: new Date(dayData.dt * 1000).toISOString(),
+                temp: dayData.main.temp.toFixed(1),
+                humidity: dayData.main.humidity.toFixed(0),
+                wind: (dayData.wind.speed * 3.6).toFixed(2),
+                icon: dayData.weather[0].icon,
+            };
+        });
+        return weatherEvents;
+    };
 
   
     //Calendar from FullCalendar
@@ -158,6 +235,7 @@
         calendar.render();
     };
 
+    //Render the toggle box with checkboxes and the corresponding label
     function renderToggleBox(uniqueEvents){
         const container = $('#toggle-box');
         container.empty();
@@ -173,10 +251,12 @@
                 .on('click', () => {
                     toggleEventDisplay(event);
                 })
-                .prop('checked', eventsOffByDefault.includes(event) ? false : true);//sets the toggles to be checked or unchecked by default
+                //sets the toggles to be checked or unchecked by default
+                .prop('checked', eventsOffByDefault.includes(event) ? false : true);
             const label = $('<label>')
                 .text(event.replaceAll('-', ' '))
                 .attr('for',`${event}`);
+            //appending them to a div inside the container so the pairs can be styled together
             const labelBoxPairDiv = $('<div>');
             labelBoxPairDiv.addClass('toggle-box-div');
             labelBoxPairDiv.append(label);
@@ -202,5 +282,18 @@
     //Event listeners
     //---------------
 
-    //updateButton.on('click', )
+    updateButton.on('click', async () => {
+        const city = cityInput.val().trim();
+        console.log(city);
+        cityInput.val('');
+        if (city === '') {
+            return; //Prevents searching if search is empty
+        }
+        saveCityToHistory(city);
+
+        const coordinates = await getCoordinates(city);
+        const weather = await getWeather(coordinates.lat, coordinates.lon);
+        const weatherEvents = getWeatherEvents(weather);
+        addWeatherToCalendar(weatherEvents);
+    });
 });
